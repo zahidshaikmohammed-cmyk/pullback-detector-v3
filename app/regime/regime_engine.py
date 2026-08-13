a2646edc26fa5e397bf983bcbf1a574c5f12846b
zahidshaikmohammed-cmyk <zahidshaikmohammed@gmail.com>
2026-08-13 13:51:04 +0530

    chore: add regime engine boundary

---
+++ app/regime/regime_engine.py
@@ -0,0 +1,4 @@
+"""V3 regime engine boundary. Implementation follows validated research rules."""
+
+def detect_regime(*args, **kwargs):
+    raise NotImplementedError("V3 regime rules not implemented yet")
