e5a11573f190033e884d4458e4efc822280b7ea2
zahidshaikmohammed-cmyk <zahidshaikmohammed@gmail.com>
2026-08-13 13:51:19 +0530

    chore: add anatomy engine boundary

---
+++ app/anatomy/engine.py
@@ -0,0 +1,6 @@
+"""V3 anatomy engine boundary."""
+
+ANATOMIES = ("failed_auction", "sweep_reclaim", "breakout_retest", "compression_expansion")
+
+def detect_anatomies(*args, **kwargs):
+    raise NotImplementedError("V3 anatomy rules not implemented yet")
