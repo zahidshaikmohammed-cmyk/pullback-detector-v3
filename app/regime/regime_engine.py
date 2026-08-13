e61f1ab734cac03cb62daa7dea8f80a5fb3bb3a9
zahidshaikmohammed-cmyk <zahidshaikmohammed@gmail.com>
2026-08-13 14:14:36 +0530

    cleanup: remove duplicate regime engine skeleton

--- app/regime/regime_engine.py
+++
@@ -1,4 +0,0 @@
-"""V3 regime engine boundary. Implementation follows validated research rules."""
-
-def detect_regime(*args, **kwargs):
-    raise NotImplementedError("V3 regime rules not implemented yet")
