c0aaffa90853a0aeb2c0db5a29636c84ef358c86
zahidshaikmohammed-cmyk <zahidshaikmohammed@gmail.com>
2026-08-13 13:52:09 +0530

    chore: add replay engine boundary

---
+++ research/replay.py
@@ -0,0 +1,4 @@
+"""Chronological replay boundary; live and replay must share detection engines."""
+
+def replay(*args, **kwargs):
+    raise NotImplementedError("V3 replay engine not implemented yet")
